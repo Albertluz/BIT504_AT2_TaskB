73b634017d02991679f6937a74034430318bf065
Albertluz <Luzaresalbert@gmail.com>
2022-09-20 16:01:19 +1200

    Changed colours of paddles, score text and ball to green.

--- Pong/src/main/PongPanel.java
+++ Pong/src/main/PongPanel.java
@@ -131,7 +131,7 @@ import javax.swing.Timer;
          Graphics2D g2d = (Graphics2D) g.create();
              Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
              g2d.setStroke(dashed);
-             g2d.setPaint(Color.WHITE);
+             g2d.setPaint(Color.GREEN);
              g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
              g2d.dispose();
       }
